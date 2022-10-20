39337e751d635b79fe718fbab1547fbf427320e1
PintoGamer64 <pintogamer64@gmail.com>
2022-10-20 16:51:32 -0500

    Delete 1 link and time to responce for 1 minute

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,15 @@ export default function App() {
   const DataBase_Local = [
     {
       URL: 'https://OokamiBot.pintogamer.repl.co'
-    },
-    {
-      URL: 'https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Classes'
     }
   ]
 
   setInterval(() => {
-    DataBase_Local.forEach( element => {
+    DataBase_Local.forEach(element => {
       const fecthData = new FecthAPI(element.URL)
       fecthData.logConsoleURL()
     });
-  }, 5000);
+  }, 60000);
 
   return (
     <div className="App">
